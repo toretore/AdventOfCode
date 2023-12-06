404b2c1d931f4cf0168aea4d8ac3f16cfa8746f6
Tore Darell <toredarell@gmail.com>
2023-12-06 09:46:47 +0100

    Day01/2: lookahead regex ftw(?)
    went through like (?=threeight) regex crates to fine one that does what
    i want.. this part was a trap for people who like to overcomplicate
    things

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -1,24 +1,57 @@
-use std::io::{self as io, Read};
+use std::{io::{self as io}, collections::HashMap};
+use fancy_regex::{Regex, Match};
 
+#[allow(unused_variables)]
 fn main() {
-    let sum = io::stdin().lines().filter_map(|line|
-      line.map(|l|
-        l.chars().filter(|c|
-          c.is_digit(10)
-        ).collect::<Vec<_>>()
-      ).ok().and_then(|ds|
-        if ds.len() > 0 {
-            String::from_iter(
-              vec![ds.first().unwrap(), ds.last().unwrap()]
-            ).parse::<i32>().ok()
-        } else {
-          None
-        }
-      )
-    ).fold(0, |s, d|
-      s + d
-    );
+    let words = HashMap::from([
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
 
-    //dbg!(sum);
+    let re = Regex::new(
+        dbg!(format!(
+            "(?=({}))",
+            words.keys().map(|k| *k ).collect::<Vec<_>>().join("|")
+        ).as_str())
+    ).unwrap();
+
+    let sum = io::stdin().lines().filter_map(|line|
+        line.ok().map(|l|
+            re.captures_iter(l.as_str()).filter_map(|cs|
+                cs.ok().and_then(|cs|
+                    cs.get(1).and_then(|m|
+                        words.get(m.as_str())
+                    )
+                )
+            ).collect::<Vec<_>>().into_iter().rfold((None,None), |(f, l), d|
+                if let Some(_) = l {
+                    (Some(d), l)
+                } else {
+                    (f, Some(d))
+                }
+            )
+      ).map(|(f,l)|
+        dbg!((dbg!(f).or(l).unwrap_or(&0) * 10) + dbg!(l).unwrap_or(&0))
+      )
+    ).sum::<i32>();
+
     println!("The sum is {sum}")
 }
